2cb72cc4fbc3a300f81cc8ae68c734ee0a04a804
Defau1 <sosinwot@mail.ru>
2026-02-16 00:36:59 +0300

    base dashboard page created antd

--- DS-front/src/components/dashboard/DashboardPage/DashboardPage.jsx
+++ DS-front/src/components/dashboard/DashboardPage/DashboardPage.jsx
@@ -1,10 +1,74 @@
-import './DashboardPage.css'
-
-function DashboardPage () {
-    return(
-        <>
-            <p>dashboard page</p>
-        </>
-    )
+import React, { useState } from 'react';
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  AppstoreAddOutlined,
+  SlidersOutlined,
+  RadarChartOutlined,
+  LineChartOutlined,
+  DollarOutlined,
+  SettingOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
 }
+const items = [
+  getItem('Дешборд', '1', <PieChartOutlined />),
+  getItem('Новая трата', '2', <AppstoreAddOutlined />),
+  getItem('История', '3', <SlidersOutlined />),
+  getItem('Категории', '4', <RadarChartOutlined />),
+  getItem('Аналитика', '5', <LineChartOutlined />),
+  getItem('Бюджеты', '6', <DollarOutlined />),
+  getItem('Личный кабинет', '7', <SettingOutlined />),
+//   getItem('User', 'sub1', <UserOutlined />, [
+//     getItem('Tom', '3'),
+//     getItem('Bill', '4'),
+//     getItem('Alex', '5'),
+//   ]),
+//   getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+//   getItem('Files', '9', <FileOutlined />),
+];
+const DashboardPage = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)}>
+        <div className="demo-logo-vertical" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }} items={[{ title: 'User' }, { title: 'Bill' }]} />
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+        Anthony ©{new Date().getFullYear()} Created by Default
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
 export default DashboardPage;
